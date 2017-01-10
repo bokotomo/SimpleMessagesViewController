542343ec4d2fde52c1aef0279fe46c42d1549d40
tomonobu <bokotomo@me.com>
2017-01-10 22:40:53 +0900

    add simplemessangerController class

---
+++ SimpleMessagesViewController/SimpleMessangerController.swift
@@ -0,0 +1,15 @@
+//
+//  SimpleMessangerController.swift
+//  SimpleMessagesViewController
+//
+//  Created by 福本 on 2017/01/10.
+//  Copyright © 2017年 Fukumoto. All rights reserved.
+//
+
+class SimpleMessangerController{
+  
+  func test(){
+    print("ok")
+  }
+  
+}
